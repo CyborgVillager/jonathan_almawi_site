4c6b6a7d7e3dd1e9305da792903f8c7b0ed017fd
CyborgVillager <almawijonathan@gmail.com>
2020-01-16 05:51:29 -0600

    heroku fix0

--- heroku_ja_site/heroku_ja_site/settings.py
+++ heroku_ja_site/heroku_ja_site/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
